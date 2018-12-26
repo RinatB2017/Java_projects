317329399c4e48dcf9bdfbbd693f02f150d76013
RinatB2017 <tux4096@gmail.com>
2018-12-26 21:59:18 +0300

    update

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -1,35 +1,90 @@
 package test;
 
-class Bot {
-    public final static int LV_FREE = 0; 
-    public final static int LV_ORGANIC_HOLD = 1;
-    public final static int LV_ORGANIC_SINK = 2;
-    public final static int LV_ALIVE = 3;
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import javax.xml.bind.DatatypeConverter;
+
+/*
+:000124FFFF000000000000FFFFFFFFFFFF0000FFFFFF000000000000000000000000FF00000000
+uint16_t MainBox::get_value(NewMoonLightPacket *packet, uint16_t address)
+{
+    union UINT16 {
+        uint16_t value;
+        struct {
+            uint8_t c:4;
+            uint8_t d:4;
+            uint8_t a:4;
+            uint8_t b:4;
+        } bytes;
+    };
+
+    union UINT16 temp;
+    temp.value = address;
+
+    return packet->body.data[temp.bytes.a][temp.bytes.b] << 8 | packet->body.data[temp.bytes.c][temp.bytes.d];
 }
+*/
 
 public class Test
 {
+    static String hex_str;
+    static byte[] data;
+    
     static void print(String text) {
         System.out.println(text);
     }
     
-    static void test(int value) {
-        switch(value) {
-            case Bot.LV_FREE:
-                break;
-                
-            case Bot.LV_ORGANIC_HOLD:
-                break;
-                
-            default:
-                break;
+    static int get_value(int address) {
+        if(address > hex_str.length()) {
+            return -1;
         }
+        String address_str = hex_str.substring(address, address+2);
+        int value = Integer.parseInt(address_str, 16);
+        return value;
     }
     
-    public static void main(String[] args) {
-        //print("Test");
-        print("value " + String.valueOf(Bot.LV_ALIVE));
+    private static boolean test(String message) throws UnsupportedEncodingException {
+        print(message);
+        hex_str = message.substring(1, message.length() - 1);
+        print(hex_str);
+        print("hex_str.length " + hex_str.length());
         
-        test(0);        
+        int addr = 0;
+        print("address " + get_value(addr));
+
+        addr+=2;
+        print("cmd " + get_value(addr));
+
+        addr+=2;
+        print("len " + get_value(addr));
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        addr = 6;
+        for(int n=addr; n<hex_str.length(); n+=2) {
+            String str = hex_str.substring(n, n+2);
+            int value = Integer.parseInt(str, 16);
+            
+            output.write((byte)value);
+        }
+        data = output.toByteArray();
+       
+        return true;
+    }
+  
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        print("Test");
+        
+        String temp_str = ":000124FFFF000000000000FFFFFFFFFFFF0000FFFFFF000000000000000000000000FF00000000\n";
+        boolean res = test(temp_str);
+        print("temp return " + res);
+        
+        print("data len " + data.length);
+        if(res) {
+            for(int n=0; n<data.length; n++) {
+                System.out.print(String.format("%x ", data[n]));
+            }
+        }
+        print("");
+        print("The end!");
     }
 }
