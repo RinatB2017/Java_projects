708f19196736c355ae5b5bc4c7694a79bf72fe3a
RinatB2017 <tux4096@gmail.com>
2019-04-18 01:22:53 +0300

    update

--- GitHub/Song-s-of-the-Space/android/gen/ru/sayakhov/songs_of_the_space/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ru.sayakhov.songs_of_the_space.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- GitHub/Song-s-of-the-Space/core/src/ru/sayakhov/songs_of_the_space/managers/IActivityRequestHandler.java
+++
@@ -1,5 +0,0 @@
-package ru.sayakhov.songs_of_the_space.managers;
-
-public interface IActivityRequestHandler {
-	public void showAds(boolean show);
-}
