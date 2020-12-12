8155b522b85ddfd41aad13066c3cfd3e6c1abaa1
tux4096@gmail.com <tux4096@gmail.com>
2020-12-12 23:00:10 +0300

    update

--- Calculator/src/main/java/com/home/calculator/Calculator.java
+++ Calculator/src/main/java/com/home/calculator/Calculator.java
@@ -4,18 +4,23 @@ package com.home.calculator;
 import java.util.*;
 
 public class Calculator {
+        String[] subStr;
+    
+    public boolean check_text(String text) {
+        String delimeter = " "; // Разделитель
+        subStr = text.split(delimeter);
+        
+        int cnt = subStr.length;
+        return (cnt == 3);
+    }
+    
     public static void main(String args[]) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter a string: ");  
         String str = sc.nextLine();
         System.out.println("You have entered: " + str);
         
-        String[] subStr;
-        String delimeter = " "; // Разделитель
-        subStr = str.split(delimeter);
-        
-        int cnt = subStr.length;
-        if(cnt != 3)
+        if(check_text(str) == false)
         {
             System.err.println("Need 3 substr");
             return;
